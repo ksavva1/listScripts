40cfb58a792ed720927225adb0c1285eab5d573b
Katie S <katiesavva1@gmail.com>
2021-12-13 23:50:55 +0000

    dataframe work

--- listsAPI.py
+++ listsAPI.py
@@ -1,6 +1,6 @@
 import requests 
 import xml.etree.ElementTree as ET #for parsing XML
-import numpy 
+import numpy
 import pandas
 
 #create dictionary for xml data to be stored in
@@ -10,7 +10,7 @@ seedPoint = input("Input seed point (x,y,z): ")
 requestURL = 'http://mouse.brain-map.org/api/v2/data/query.xml?criteria=service::mouse_agea%5Bset$eq%27mouse_coronal%27%5D%5Bseed_point$eq'+seedPoint+'%5D%5Bseed_age$eqP56%5D%5Bmap_age$eqP56%5D%5Bcorrelation_threshold1$eq0.7%5D%5Bthreshold1$eq%271,50%27%5D%5Bcorrelation_threshold2$eq0.8799999999999999%5D%5Bthreshold2$eq%270,50%27%5D%5Bstart_row$eq0%5D%5Bnum_rows$eq2000%5D'
 
 r = requests.get(requestURL)
-root = ET.fromstring(r.content)  #paarse XML
+root = ET.fromstring(r.content) #paarse XML
 
 geneIDList = []
 foldChangeList = []
