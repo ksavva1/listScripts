847f4622815425148ae8c583dc0bf3c37a08bc38
Katie S <katiesavva1@gmail.com>
2021-12-13 15:11:42 +0000

    first commit

---
+++ listDownload.py
@@ -0,0 +1,4 @@
+import requests
+domain = input()
+domainoutput = requests.get("http://mouse.brain-map.org/api/v2/data/query.xml?criteria=service::mouse_agea%5Bset$eq%27mouse_coronal%27%5D%5Bseed_point$eq"+domain+"%5D%5Bseed_age$eqP56%5D%5Bmap_age$eqP56%5D%5Bcorrelation_threshold1$eq0.7%5D%5Bthreshold1$eq%271,50%27%5D%5Bcorrelation_threshold2$eq0.8799999999999999%5D%5Bthreshold2$eq%270,50%27%5D%5Bstart_row$eq0%5D%5Bnum_rows$eq2000%5D")
+print(domainoutput.text)
