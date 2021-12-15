8a7025c1f6bd923c60888b7fcf2c4a6826714c82
Katie S <katiesavva1@gmail.com>
2021-12-15 00:02:53 +0000

    test run

--- listsAPI.py
+++ listsAPI.py
@@ -1,5 +1,5 @@
 import requests 
-import xml.etree.ElementTree as ET #for parsing XML
+import xml.etree.ElementTree as ET
 import numpy as np
 import pandas as pd
 
@@ -10,20 +10,25 @@ seedPoint = input("Input seed point (x,y,z): ")
 requestURL = 'http://mouse.brain-map.org/api/v2/data/query.xml?criteria=service::mouse_agea%5Bset$eq%27mouse_coronal%27%5D%5Bseed_point$eq'+seedPoint+'%5D%5Bseed_age$eqP56%5D%5Bmap_age$eqP56%5D%5Bcorrelation_threshold1$eq0.7%5D%5Bthreshold1$eq%271,50%27%5D%5Bcorrelation_threshold2$eq0.8799999999999999%5D%5Bthreshold2$eq%270,50%27%5D%5Bstart_row$eq0%5D%5Bnum_rows$eq2000%5D'
 
 r = requests.get(requestURL)
-root = ET.fromstring(r.content) #paarse XML
+root = ET.fromstring(r.content) #parse XML
 
 geneIDList = []
 foldChangeList = []
 
-for geneID in root.inter('gene-id'):
-    geneIDList.append(geneID)
-    print(geneID)
+for geneID in root.iter('gene-id'):
+    geneIDList.extend(geneID)
+    
 
-for foldChange in root.inter('fold-change'):
-    foldChangeList.append(foldChange)
+for foldChange in root.iter('fold-change'):
+    foldChangeList.extend(foldChange)
+    print(foldChange)
 
-geneDF = pd.DataFRame({'geneID':geneIDList, 'foldChange':foldChange}) #convert list to dataframe
+geneDF = pd.DataFrame({'geneID':geneIDList, 'foldChange':foldChangeList}) #convert list to dataframe
 
-geneDF\
-.style.set_properties(subset=['geneID']) \
-.hide_index() #show dataframe
+#geneDF\
+#.style.set_properties(subset=['geneID']) \
+#.hide_index() #show dataframe
+
+print(geneDF)
+
+#not getting raw data from xml. maybe look into using soup again?
