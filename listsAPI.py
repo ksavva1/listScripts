83cfef2848110dbb37a06c04a21b6cf92b7337af
Katie S <katiesavva1@gmail.com>
2021-12-16 01:07:47 +0000

    Data type issues fixed (?)

--- listsAPI.py
+++ listsAPI.py
@@ -5,20 +5,35 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 seedPoint = input("Input seed point (x,y,z): ") #6800,4200,5600
-requestURL = 'http://mouse.brain-map.org/api/v2/data/query.xml?criteria=service::mouse_agea%5Bset$eq%27mouse_coronal%27%5D%5Bseed_point$eq'+seedPoint+'%5D%5Bseed_age$eqP56%5D%5Bmap_age$eqP56%5D%5Bcorrelation_threshold1$eq0.7%5D%5Bthreshold1$eq%271,50%27%5D%5Bcorrelation_threshold2$eq0.8799999999999999%5D%5Bthreshold2$eq%270,50%27%5D%5Bstart_row$eq0%5D%5Bnum_rows$eq2000%5D'
+requestURL = 'http://mouse.brain-map.org/api/v2/data/query.xml?criteria=service::mouse_agea%5Bset$eq%27mouse_coronal%27%5D%5Bseed_point$eq'+seedPoint+'%5D%5Bseed_age$eqP56%5D%5Bmap_age$eqP56%5D%5Bcorrelation_threshold1$eq0.7%5D%5Bthreshold1$eq%271,50%27%5D%5Bcorrelation_threshold2$eq0.8799999999999999%5D%5Bthreshold2$eq%270,50%27%5D%5Bstart_row$eq0%5D%5Bnum_rows$eq5000%5D'
 
 r = requests.get(requestURL)
 soup = BeautifulSoup(r.content, 'html.parser')
 list(soup.children)
 
+#get data
 geneIDList = soup.find_all('gene-id')
-foldChangeList = soup.find_all('fold-change')
 geneSymbolList = soup.find_all('gene-symbol')
+fold = soup.find_all('fold-change')
+
+foldChangeList = []
+
+for i in fold:
+    i = str(i)
+    temp = i[:-14]
+    temp2 = temp[13:]
+    foldChangeList.append(temp2)
+print(foldChangeList)
 
 geneDF = pd.DataFrame({'geneID':geneIDList, 'geneSymbol':geneSymbolList, 'foldChange':foldChangeList}) #convert list to dataframe
-geneDF['foldChange'].astype('float')
 
-tomato = geneDF['foldChange'].between(0.9, 1.1, inclusive=False)
+print(geneDF.dtypes)
+#CHANGE DT of FOLD CHANGE TO INT
 
-#print(geneDF)
-print(tomato)
+#-----------------data splicing methods-----------------------------
+#tomato = geneDF['foldChange'].between(0.9, 1.1, inclusive=False)
+#tomato = geneDF[(geneDF['foldChange'] > 0) & (geneDF['foldChange'] < 14)]
+#print(tomato)
+
+#print(geneDF.dtypes)
+print(geneDF)
