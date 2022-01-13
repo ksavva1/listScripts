66393d8d4cc8d9791e95c32dd6fc86ba5bc6234a
Katie S <katiesavva1@gmail.com>
2022-01-13 21:45:01 +0000

    Get array from mhd file

--- foldCalculator.py
+++ foldCalculator.py
@@ -1,14 +1,12 @@
-import cv2, os, webbrowser
+import os, webbrowser, zipfile, time
+import SimpleITK as sitk
 import numpy as np
-import zipfile
-import time
 
 #download and unzip file
 for j in range(123,127,1):
     requestURL = 'http://api.brain-map.org/grid_data/download/' + str(j)
     webbrowser.open(requestURL)
-
-    time.sleep(6)
+    time.sleep(6) #to give time for file to actually download
 
     dir_name = '/Users/katiesavva/Downloads'
     extension = ".zip"
@@ -21,33 +19,38 @@ for j in range(123,127,1):
             zipRef.extractall(dir_name) 
             zipRef.close()
             os.remove(fileName) #delete zipped file
-
+    
+    #rename energy files
     oldName = 'energy.mhd'
     newName = str(j) + '.mhd'
     os.rename(oldName, newName)
 
-#save intensity data as an array
-img = cv2.imread('path/to/img', 1)
+
+    #save intensity data as an array
+    itkImage = sitk.ReadImage(newName)
+    #convert the image to a numpy array and shuffle dimensions to get axis in the order z,y,x
+    sitkArray = sitk.GetArrayFromImage(itkImage)
+
 
 #function to get expression energy
-def expression(i,x,y,z):
+#def expression(i,x,y,z):
     #rip expression values out of file or matrix
 
 
 #loop through all points in downloaded file
-for z1 in zAxis:
-    for z2 in zAxis:
-        for y1 in yAxis:
-            for y2 in yAxis:
-                for x1 in xAxis:
-                    for x2 in xAxis:
+#for z1 in zAxis:
+ #   for z2 in zAxis:
+  #      for y1 in yAxis:
+   #         for y2 in yAxis:
+    #            for x1 in xAxis:
+     #               for x2 in xAxis:
                         #coordinates for seed and target points
-                        seed = str(x1) + "," + str(y1) + "," + str(z1)
-                        target = str(x2) + "," + str(y2) + "," + str(z2)
+       #                 seed = str(x1) + "," + str(y1) + "," + str(z1)
+        #                target = str(x2) + "," + str(y2) + "," + str(z2)
                         #open file for that location
-                        f = open('%s.csv' % seed, 'a')
+         #               f = open('%s.csv' % seed, 'a')
                         #for every gene present, compare expression values in the 2 locations = fold change
-                        foldChange = expression(i,x1,y1,z1) / expression(i,x2,y2,z2)
+          #              foldChange = expression(i,x1,y1,z1) / expression(i,x2,y2,z2)
                         #write fold change, gene id, seed & target voxel to location file
-                        f.write("Target: " + target + " ID: " + geneID + " Fold Change: " + foldChange)
-                        f.close()
+           #             f.write("Target: " + target + " ID: " + geneID + " Fold Change: " + foldChange)
+                #        f.close()
