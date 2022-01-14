121075239e5e0d0f8650dada5d336edb261f6f15
Katie S <katiesavva1@gmail.com>
2022-01-14 01:24:44 +0000

    loops and comparisons

--- foldCalculator.py
+++ foldCalculator.py
@@ -2,9 +2,9 @@ import os, webbrowser, zipfile, time
 import SimpleITK as sitk
 import numpy as np
 
-#download and unzip file
-for j in range(123,127,1):
-    requestURL = 'http://api.brain-map.org/grid_data/download/' + str(j)
+#download and get array from energy.mhd
+def getArray(dlCounter):
+    requestURL = 'http://api.brain-map.org/grid_data/download/' + str(dlCounter)
     webbrowser.open(requestURL)
     time.sleep(6) #to give time for file to actually download
 
@@ -22,37 +22,42 @@ for j in range(123,127,1):
     
     #rename energy files
     oldName = 'energy.mhd'
-    newName = str(j) + '.mhd'
+    newName = str(dlCounter) + '.mhd'
     os.rename(oldName, newName)
 
     #save intensity data as an array
     itkImage = sitk.ReadImage(newName)
     #convert the image to a numpy array and shuffle dimensions to get axis in the order z,y,x
     sitkArray = sitk.GetArrayFromImage(itkImage)
-    print(sitkArray)
+    return sitkArray, fileName
 
-    f = open('%s.txt' % str(j), "a") 
-    f.write(sitkArray)
+#function to get expression energy out of seed and target arrays
+def expression(w,x,y,z):
+    return w[x][y][z]
 
-#function to get expression energy
-#def expression(i,x,y,z):
-    #rip expression values out of file or matrix
+targetCounter = 124
+
+for i in range(123,127,1): #change to 0,max,1 after tests are done and add clause for when the url doesnt download anything
+    seedArray, seedName = getArray(i)
+    targetArray, targetName = getArray(targetCounter) # +1 after each round of comparisons to compare the og array with the next array.
 
 
-#loop through all points in downloaded file
-#for z1 in zAxis:
- #   for z2 in zAxis:
-  #      for y1 in yAxis:
-   #         for y2 in yAxis:
-    #            for x1 in xAxis:
-     #               for x2 in xAxis:
-                        #coordinates for seed and target points
-       #                 seed = str(x1) + "," + str(y1) + "," + str(z1)
-        #                target = str(x2) + "," + str(y2) + "," + str(z2)
-                        #open file for that location
-         #               f = open('%s.csv' % seed, 'a')
-                        #for every gene present, compare expression values in the 2 locations = fold change
-          #              foldChange = expression(i,x1,y1,z1) / expression(i,x2,y2,z2)
-                        #write fold change, gene id, seed & target voxel to location file
-           #             f.write("Target: " + target + " ID: " + geneID + " Fold Change: " + foldChange)
-                #        f.close()
+    #loop through all points in downloaded file
+    for z1 in zAxis:
+        for z2 in zAxis:
+            for y1 in yAxis:
+                for y2 in yAxis:
+                    for x1 in xAxis:
+                        for x2 in xAxis:
+                            #coordinates for seed and target points
+                            seed = str(x1) + "," + str(y1) + "," + str(z1)
+                            target = str(x2) + "," + str(y2) + "," + str(z2)
+                            #open file for that location
+                            f = open('%s.csv' % seedName, 'a')
+                            #for every gene present, compare expression values in the 2 locations = fold change
+                            foldChange = expression(seedArray,x1,y1,z1) / expression(targetArray,x2,y2,z2)
+                            #write fold change, gene id, seed & target voxel to location file
+                            f.write("Target: " + targetName + " Experiment ID: " + str(i) + "Seed Coordinates: " + seed + "Target Coordinates: " + target + " Fold Change: " + str(foldChange)
+                            f.close()
+
+    targetCounter += 1
